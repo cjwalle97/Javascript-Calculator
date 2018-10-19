300c73ba3d09970a16b856aa18dabd59355686dd
cjwalle97 <cjwalle97@gmail.com>
2018-10-19 14:47:12 -0500

    Update
    Update

--- Javascript Calculator/js/main.js
+++ Javascript Calculator/js/main.js
@@ -1,1 +1,14 @@
 ﻿// Your code here!
+window.onload = function ()
+{
+    document.getElementById("Add").onclick = function (_evt)
+    {
+        
+    }
+}
+
+function Addition()
+{
+    
+    var messageDialog = new Windows.UI.Popups.MessageDialog("Here is your result", )
+}
