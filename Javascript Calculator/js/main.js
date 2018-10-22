93c810ba556aeae3aa5bd009fded73c5dc3114cc
cjwalle97 <cjwalle97@gmail.com>
2018-10-22 11:51:45 -0500

    Update #1 10/22/18
    Update

--- Javascript Calculator/js/main.js
+++ Javascript Calculator/js/main.js
@@ -1,16 +1,15 @@
 ﻿// Your code here!
 window.onload = function ()
 {
+    
     document.getElementById("Add").onclick = function (_evt)
     {
-        Addition()
+        Addition(nx, ny)
     }
 }
 
-function Addition()
+function Addition(a, b)
 {
-    var a = 1;
-    var b = 2;
     var c = a + b;
     var messageDialog = new Windows.UI.Popups.MessageDialog("Here is your result ", c);
     messageDialog.showAsync();
